4a805f42583b61214e7513796f1534ea4d0c3af4
zhannagribanova <zzzgggrrriii@gmail.com>
2021-01-30 23:31:55 +0300

    Реализован первый тест в Mantis - логин

---
+++ fixture/application.py
@@ -0,0 +1,32 @@
+from selenium import webdriver
+from fixture.session import SessionHelper
+
+
+class Application:
+
+    def __init__(self, browser, base_URL):
+        if browser == "firefox":
+            self.wd = webdriver.Firefox()
+        elif browser == "chrome":
+            self.wd = webdriver.Chrome()
+        elif browser == "ie":
+            self.wd = webdriver.Ie()
+        else:
+            raise ValueError("Unrecognized browser %s" % browser)
+        self.wd.implicitly_wait(5)
+        self.session = SessionHelper(self)
+        self.base_URL = base_URL
+
+    def is_valid(self):
+        try:
+            self.wd.current_url
+            return True
+        except:
+            return False
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get(self.base_URL)
+
+    def destroy(self):
+        self.wd.quit()
